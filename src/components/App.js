346b86aa7d71d589dcb73a4c938aa762358c070d
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-23 23:16:09 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,26 @@ import axios from 'axios';
 
 export default function App() {
 
+    const [hits, SetHits] = useState([]);
+
     useEffect(() => {
-        axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+        axios.get('http://hn.algolia.com/api/v1/search?query=javascript')
             .then(res => {
-                console.log(res.data);
+                SetHits(res.data.hits);
             })
-    })
+    }, [])
 
     return(
         <div>
-
+            <ul>
+                {
+                hits.map(hit => (
+                    <li key={hit.objectID}>
+                        <a href={hit.url}>{hit.title}</a>
+                    </li>
+                ))
+                }
+            </ul>
         </div>
     );
 }
