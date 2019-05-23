337e1b14664e76a7783626ad67f80ae541cf08f7
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-23 23:37:26 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,28 @@ import axios from 'axios';
 export default function App() {
 
     const [hits, SetHits] = useState([]);
+    const [keyword, SetKeyword] = useState("javascript");
 
     useEffect(() => {
         GetHits();
-    }, []);
+    }, [keyword]);
 
     const GetHits = async () => {
-        const res = await axios.get('http://hn.algolia.com/api/v1/search?query=javascript');
+        const res = await axios.get(`http://hn.algolia.com/api/v1/search?query=${keyword}`);
         
         SetHits(res.data.hits);
     }
 
     return(
         <div>
+            <form className="pure-form">
+                <fieldset>
+                    <input type="query" placeholder="Search here" onChange={event => GetHits(event.target.value)}/>
+
+                    <button type="submit" className="pure-button pure-button-primary">Search</button>
+                </fieldset>
+            </form>
+
             <ul>
                 {
                 hits.map(hit => (
