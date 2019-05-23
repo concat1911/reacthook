57f46c7a325b159921a8ee341c8c004112f33e33
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-23 23:52:03 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 
 export default function App() {
 
     const [hits, SetHits] = useState([]);
     const [keyword, SetKeyword] = useState("javascript");
+    const searchInput = useRef();
 
     useEffect(() => {
         GetHits();
-    }, [keyword]);
+    }, []);
 
     const GetHits = async () => {
-        const res = await axios.get(`http://hn.algolia.com/api/v1/search?query=${keyword}`);
-        
+        const res = await axios.get(`http://hn.algolia.com/api/v1/search?query=${keyword}`);    
         SetHits(res.data.hits);
+    }
+
+    const HandleSearch = event => {
+        event.preventDefault();
+        GetHits();
+    }
+
+    const HandleClear = () => {
+        SetKeyword("");
+        searchInput.current.focus();
     }
 
     return(
         <div>
-            <form className="pure-form">
+            <form className="pure-form" onSubmit={HandleSearch}>
                 <fieldset>
-                    <input type="query" placeholder="Search here" onChange={event => GetHits(event.target.value)}/>
+                    <input type="query" placeholder="Search here" onChange={event => SetKeyword(event.target.value)} value={keyword} ref={searchInput}/>
 
                     <button type="submit" className="pure-button pure-button-primary">Search</button>
+                    <button type="button" className="pure-button pure-button-primary" onClick={HandleClear}>Clear</button>
                 </fieldset>
             </form>
 
